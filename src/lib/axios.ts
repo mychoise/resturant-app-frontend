013b0c1e13b4f0a55b1ab12ccf7fc513bc22a98a
mychoise <dklsabin23@gmail.com>
2026-07-30 09:44:59 +0545

    updated

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://192.168.1.71:3000/", // point to your deployed backend
+  baseURL: "https://resturant-app-production-859a.up.railway.app/", // point to your deployed backend
   withCredentials: true, // if using cookies
 });
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/*<ReactQueryDevtools initialIsOpen={false} />*/}
     </BrowserRouter>
   </QueryClientProvider>,
 );
